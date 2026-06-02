bc06b2a18fbbebb6e8ba87a997b8dcaca63cd561
Nogringo <npub1kg4sdvz3l4fr99n2jdz2vdxe2mpacva87hkdetv76ywacsfq5leqquw5te@uid.ovh>
2026-06-02 17:07:24 +0200

    first commit

---
+++ bin/scheduler_dvm.dart
@@ -0,0 +1,5 @@
+import 'package:scheduler_dvm/scheduler_dvm.dart' as scheduler_dvm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${scheduler_dvm.calculate()}!');
+}
---
+++ test/scheduler_dvm_test.dart
@@ -0,0 +1,8 @@
+import 'package:scheduler_dvm/scheduler_dvm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
